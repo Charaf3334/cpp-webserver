81a1961fe3a0a5366decb65a4aaf1c7dc3693171
mid-taoufiq <taoufiqibariken@gmail.com>
2025-12-18 22:26:34 +0100

    compile fix

--- Server.cpp
+++ Server.cpp
@@ -675,7 +675,7 @@ std::string simplifyPath(std::string path) {
 	std::string *words = split_path(path, &count);
 	std::string correct_path;
 	std::stack <std::string> s;
-	for (int i = 0; i < count; i++)
+	for (size_t i = 0; i < count; i++)
 	{
         if (words[i] == "..")
         {
@@ -690,8 +690,6 @@ std::string simplifyPath(std::string path) {
             s.push(words[i]);
 		}
 	}
-    if (s.empty())
-        return "/";
 	while (!s.empty())
 	{
 		correct_path.insert(0, "/" + s.top());
